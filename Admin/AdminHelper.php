3542cd856d73736e409ce9b12a6394cc4048e278
Marcos Fadul <marcos@polargold.de>
2016-05-22 20:05:22 +0200

    Minor: Import Nested Field fix from Master

--- Admin/AdminHelper.php
+++ Admin/AdminHelper.php
@@ -16,13 +16,22 @@ use Doctrine\Common\Util\ClassUtils;
 use Sonata\AdminBundle\Exception\NoValueException;
 use Sonata\AdminBundle\Util\FormBuilderIterator;
 use Sonata\AdminBundle\Util\FormViewIterator;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormView;
-
+use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
+use Symfony\Component\PropertyAccess\Exception\UnexpectedTypeException;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+/**
+ * Class AdminHelper.
+ *
+ * @author  Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ */
 class AdminHelper
 {
+    /**
+     * @var Pool
+     */
     protected $pool;
-
     /**
      * @param Pool $pool
      */
@@ -30,31 +39,28 @@ class AdminHelper
     {
         $this->pool = $pool;
     }
-
     /**
      * @throws \RuntimeException
      *
-     * @param \Symfony\Component\Form\FormBuilder $formBuilder
+     * @param FormBuilderInterface $formBuilder
      * @param string                              $elementId
      *
-     * @return \Symfony\Component\Form\FormBuilder
+     * @return FormBuilderInterface
      */
-    public function getChildFormBuilder(FormBuilder $formBuilder, $elementId)
+    public function getChildFormBuilder(FormBuilderInterface $formBuilder, $elementId)
     {
         foreach (new FormBuilderIterator($formBuilder) as $name => $formBuilder) {
             if ($name == $elementId) {
                 return $formBuilder;
             }
         }
-
         return;
     }
-
     /**
-     * @param \Symfony\Component\Form\FormView $formView
+     * @param FormView $formView
      * @param string                           $elementId
      *
-     * @return null|\Symfony\Component\Form\FormView
+     * @return null|FormView
      */
     public function getChildFormView(FormView $formView, $elementId)
     {
@@ -63,22 +69,19 @@ class AdminHelper
                 return $formView;
             }
         }
-
         return;
     }
-
     /**
      * @deprecated
      *
      * @param string $code
      *
-     * @return \Sonata\AdminBundle\Admin\AdminInterface
+     * @return AdminInterface
      */
     public function getAdmin($code)
     {
         return $this->pool->getInstance($code);
     }
-
     /**
      * Note:
      *   This code is ugly, but there is no better way of doing it.
@@ -87,7 +90,7 @@ class AdminHelper
      *
      * @throws \RuntimeException
      *
-     * @param \Sonata\AdminBundle\Admin\AdminInterface $admin
+     * @param AdminInterface $admin
      * @param object                                   $subject
      * @param string                                   $elementId
      *
@@ -97,65 +100,69 @@ class AdminHelper
     {
         // retrieve the subject
         $formBuilder = $admin->getFormBuilder();
-
         $form = $formBuilder->getForm();
         $form->setData($subject);
-        $form->submit($admin->getRequest());
-
+        $form->handleRequest($admin->getRequest());
         // get the field element
         $childFormBuilder = $this->getChildFormBuilder($formBuilder, $elementId);
-
+        //Child form not found (probably nested one)
+        //if childFormBuilder was not found resulted in fatal error getName() method call on non object
+        if (!$childFormBuilder) {
+            $propertyAccessor = $this->pool->getPropertyAccessor();
+            $entity = $admin->getSubject();
+            $path = $this->getElementAccessPath($elementId, $entity);
+            $collection = $propertyAccessor->getValue($entity, $path);
+            if ($collection instanceof \Doctrine\ORM\PersistentCollection || $collection instanceof \Doctrine\ODM\MongoDB\PersistentCollection) {
+                //since doctrine 2.4
+                $entityClassName = $collection->getTypeClass()->getName();
+            } elseif ($collection instanceof \Doctrine\Common\Collections\Collection) {
+                $entityClassName = $this->getEntityClassName($admin, explode('.', preg_replace('#\[\d*?\]#', '', $path)));
+            } else {
+                throw new \Exception('unknown collection class');
+            }
+            $collection->add(new $entityClassName());
+            $propertyAccessor->setValue($entity, $path, $collection);
+            $fieldDescription = null;
+        } else {
         // retrieve the FieldDescription
         $fieldDescription = $admin->getFormFieldDescription($childFormBuilder->getName());
-
         try {
             $value = $fieldDescription->getValue($form->getData());
         } catch (NoValueException $e) {
             $value = null;
         }
-
         // retrieve the posted data
         $data = $admin->getRequest()->get($formBuilder->getName());
-
         if (!isset($data[$childFormBuilder->getName()])) {
             $data[$childFormBuilder->getName()] = array();
         }
-
         $objectCount = count($value);
         $postCount   = count($data[$childFormBuilder->getName()]);
-
         $fields = array_keys($fieldDescription->getAssociationAdmin()->getFormFieldDescriptions());
-
         // for now, not sure how to do that
         $value = array();
         foreach ($fields as $name) {
             $value[$name] = '';
         }
-
         // add new elements to the subject
         while ($objectCount < $postCount) {
             // append a new instance into the object
             $this->addNewInstance($form->getData(), $fieldDescription);
             ++$objectCount;
         }
-
         $this->addNewInstance($form->getData(), $fieldDescription);
-        $data[$childFormBuilder->getName()][] = $value;
-
+        }
         $finalForm = $admin->getFormBuilder()->getForm();
         $finalForm->setData($subject);
-
         // bind the data
         $finalForm->setData($form->getData());
-
         return array($fieldDescription, $finalForm);
     }
-
     /**
      * Add a new instance to the related FieldDescriptionInterface value.
      *
      * @param object                                              $object
-     * @param \Sonata\AdminBundle\Admin\FieldDescriptionInterface $fieldDescription
+     * @param FieldDescriptionInterface $fieldDescription
      *
      * @throws \RuntimeException
      */
@@ -163,24 +170,18 @@ class AdminHelper
     {
         $instance = $fieldDescription->getAssociationAdmin()->getNewInstance();
         $mapping  = $fieldDescription->getAssociationMapping();
-
-        $method = sprintf('add%s', $this->camelize($mapping['fieldName']));
-
+        $method = sprintf('add%s', Inflector::classify($mapping['fieldName']));
         if (!method_exists($object, $method)) {
             $method = rtrim($method, 's');
-
             if (!method_exists($object, $method)) {
-                $method = sprintf('add%s', $this->camelize(Inflector::singularize($mapping['fieldName'])));
-
+                $method = sprintf('add%s', Inflector::classify(Inflector::singularize($mapping['fieldName'])));
                 if (!method_exists($object, $method)) {
                     throw new \RuntimeException(sprintf('Please add a method %s in the %s class!', $method, ClassUtils::getClass($object)));
                 }
             }
         }
-
         $object->$method($instance);
     }
-
     /**
      * Camelize a string.
      *
@@ -189,9 +190,94 @@ class AdminHelper
      * @param string $property
      *
      * @return string
+     *
+     * @deprecated Deprecated since version 3.1. Use \Doctrine\Common\Inflector\Inflector::classify() instead.
      */
     public function camelize($property)
     {
-        return BaseFieldDescription::camelize($property);
+        @trigger_error(
+            sprintf(
+                'The %s method is deprecated since 3.1 and will be removed in 4.0. '.
+                'Use \Doctrine\Common\Inflector\Inflector::classify() instead.',
+                __METHOD__
+            ),
+            E_USER_DEPRECATED
+        );
+        return Inflector::classify($property);
+    }
+    /**
+     * Get access path to element which works with PropertyAccessor.
+     *
+     * @param string $elementId expects string in format used in form id field. (uniqueIdentifier_model_sub_model or uniqueIdentifier_model_1_sub_model etc.)
+     * @param mixed  $entity
+     *
+     * @return string
+     *
+     * @throws \Exception
+     */
+    public function getElementAccessPath($elementId, $entity)
+    {
+        $propertyAccessor = $this->pool->getPropertyAccessor();
+        $idWithoutIdentifier = preg_replace('/^[^_]*_/', '', $elementId);
+        $initialPath = preg_replace('#(_(\d+)_)#', '[$2]_', $idWithoutIdentifier);
+        $parts = explode('_', $initialPath);
+        $totalPath = '';
+        $currentPath = '';
+        foreach ($parts as $part) {
+            $currentPath .= empty($currentPath) ? $part : '_'.$part;
+            $separator = empty($totalPath) ? '' : '.';
+            if ($this->pathExists($propertyAccessor, $entity, $totalPath.$separator.$currentPath)) {
+                $totalPath .= $separator.$currentPath;
+                $currentPath = '';
+}
+        }
+        if (!empty($currentPath)) {
+            throw new \Exception(sprintf('Could not get element id from %s Failing part: %s', $elementId, $currentPath));
+        }
+        return $totalPath;
+    }
+    /**
+     * Recursively find the class name of the admin responsible for the element at the end of an association chain.
+     *
+     * @param AdminInterface $admin
+     * @param array          $elements
+     *
+     * @return string
+     */
+    protected function getEntityClassName(AdminInterface $admin, $elements)
+    {
+        $element = array_shift($elements);
+        $associationAdmin = $admin->getFormFieldDescription($element)->getAssociationAdmin();
+        if (count($elements) == 0) {
+            return $associationAdmin->getClass();
+        } else {
+            return $this->getEntityClassName($associationAdmin, $elements);
+        }
+    }
+    /**
+     * Check if given path exists in $entity.
+     *
+     * @param PropertyAccessorInterface $propertyAccessor
+     * @param mixed                     $entity
+     * @param string                    $path
+     *
+     * @return bool
+     *
+     * @throws \RuntimeException
+     */
+    private function pathExists(PropertyAccessorInterface $propertyAccessor, $entity, $path)
+    {
+        // Symfony <= 2.3 did not have isReadable method for PropertyAccessor
+        if (method_exists($propertyAccessor, 'isReadable')) {
+            return $propertyAccessor->isReadable($entity, $path);
+        }
+        try {
+            $propertyAccessor->getValue($entity, $path);
+            return true;
+        } catch (NoSuchPropertyException $e) {
+            return false;
+        } catch (UnexpectedTypeException $e) {
+            return false;
+        }
     }
 }
